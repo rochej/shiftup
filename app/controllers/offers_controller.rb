2fc3f20d66edc93d924ad79a8f6194a7e583b80c
Roche <rochej@gmail.com>
2016-04-06 14:52:56 -0700

    Update shifts_controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,12 +1,11 @@
 class OffersController < ApplicationController
   def create
     @shift = Shift.find(params[:shift_id])
-    @user = current_user
-    Offer.where("offerer_id=? AND shift_id=?", @user.id, @shift.id).delete_all
+    Offer.where("offerer_id=? AND shift_id=?", current_user.id, @shift.id).delete_all
     @offer = Offer.new(type: params["type"])
     @offer.shift = @shift
-    @shift.covered = true if @offer.type == "yes"
     @offer.offerer = current_user
+    @shift.yes_offers?
     @shift.save
     @offer.save
     render json: @offer
