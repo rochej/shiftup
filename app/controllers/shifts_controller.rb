bbf9457594cd48278b613ed87bcb6599eff11b9a
Roche <rochej@gmail.com>
2016-04-05 21:35:25 -0700

    Add shift form

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -8,4 +8,17 @@ def index
   end
 end
 
+def create
+  date = params[:date]
+  time = params[:time]
+  datetime = Date.parse(date + " " + time)
+  @shift = Shift.create(datetime: datetime, giver: current_user, covered: false)
+  @shifts = Shift.all
+  respond_to do |format|
+    format.html { render 'index' }
+    format.json { render json: @shifts}
+  end
+end
+
+
 end
