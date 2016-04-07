d117264f275d6904efe0636bb7f765dd51c8dee7
Roche <rochej@gmail.com>
2016-04-07 10:43:23 -0700

    Update offers controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,8 +1,7 @@
 class OffersController < ApplicationController
   def create
     @shift = Shift.find(params[:shift_id])
-    @user = current_user
-    Offer.where("offerer_id=? AND shift_id=?", @user.id, @shift.id).delete_all
+    @shift.offers.where(offerer_id: current_user.id).delete_all
     @offer = Offer.new(type: params["type"])
     @offer.shift = @shift
     @shift.covered = true if @offer.type == "yes"
