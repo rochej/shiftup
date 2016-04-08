afd1da0211ca445be7421d974464f0181b5d5a65
Roche <rochej@gmail.com>
2016-04-07 21:40:46 -0700

    Add sketchy csrf disabling

--- app/controllers/get_texts_controller.rb
+++ app/controllers/get_texts_controller.rb
@@ -1,6 +1,12 @@
 class GetTextsController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   def get_text
+    message_body = params['Body']
+    from_number = params['From']
+    p "&" * 80
+    p params
 
   end
 
